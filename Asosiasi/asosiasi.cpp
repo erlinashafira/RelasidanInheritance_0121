5157d1ba1636e8048bbcf9011782d1604c4c3745
errerew <erlina31acc@gmail.com>
2025-05-27 18:53:49 +0700

    membuat asosiasi antar dokter dan pasien

---
+++ Asosiasi/asosiasi.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien{
+    public:
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama): nama (pNama){
+            cout << "Pasien \"" << nama << "\" ada\n";
+        }
+
+        ~pasien(){
+            cout << "pasien \"" << nama
+            << "\" tidak ada\n";
+        }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Daftar \"" << nama << "\" ada\n";
+        }
+        ~dokter() {
+            cout << "Dokter \"" << nama <<
+            << "\" tidak ada\n";
+        }
+        void tambahPasien(pasien*);
+        void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter(){
+    cout << "Daftar dokter yang menangani pasien \""
+    << ibu->nama << "\":\n";
+    //auto digunakan
+    for (auto a : daftar_dokter){
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
