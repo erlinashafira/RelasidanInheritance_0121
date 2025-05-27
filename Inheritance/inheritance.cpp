1946f99004f9ba619454086a064e956329c4994b
errerew <erlina31acc@gmail.com>
2025-05-27 20:05:13 +0700

    menambahkan class orang dengan member nama

---
+++ Inheritance/inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+    public:
+        string nama;
+
+        orang(string pNama):
+            nama(pNama) {
+                cout << "orang dibuat\n" << endl;
+            }
+            ~orang() {
+                cout << "orang dihapus\n" << endl;
+            }
+
+            int jumlah(int a, int b){
+                return a + b;
+            }
+};
+
+class pelajar : public orang {
+    public:
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+        ~pelajar() {
+            cout << "pelajar dihapus\n" << endl;
+        }
+        string perkenalan(){
+            return "hallo, nama saya" + nama + "dari sekolah" + sekolah + "\n"}
+        };
